c08b671113154fbfd246d7a5badae393916940be
Sarah Samiha Zaman <sarahfaizakhan@gmail.com>
2023-10-23 03:25:01 +0600

    delete from cart functionality added

--- index.js
+++ index.js
@@ -115,6 +115,22 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id }
+            const result = await cartCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: id }
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
         
 
 
